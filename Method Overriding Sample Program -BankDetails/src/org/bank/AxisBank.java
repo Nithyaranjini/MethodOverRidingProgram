d5e271c4728f511051584d699f1eeef41da3ea90
Nithyaranjini <srinithya2k@gmail.com>
2023-02-27 18:07:25 +0530

    commit method overriding program

---
+++ Method Overriding Sample Program -BankDetails/src/org/bank/AxisBank.java
@@ -0,0 +1,19 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	@Override
+	public void deposit() {
+		System.out.println("my deposit account is :63585797585");}
+		
+	public static void main(String[] args) {
+		AxisBank a = new AxisBank();
+		a.deposit();
+		a.saving();
+		a.fixed();
+		
+	}}
+	
+
+
+
+
---
+++ Method Overriding Sample Program -BankDetails/src/org/bank/BankInfo.java
@@ -0,0 +1,18 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+	 
+	System.out.println("my saving account is:562789602" );}
+	public  void fixed() {
+	  System.out.println("my fixed amount is 63748378247587.653648376");}
+	public void deposit() {
+		System.out.println("my deposit account is 56563.6537899");}
+		
+	
+
+	
+
+	}
+
+
